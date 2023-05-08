3eafeced15d4d51200673e81e7335e177a950a61
Mert Bingol <mertbingol00@protonmail.com>
2023-05-09 02:04:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/disk-status.html')
 def disk_analyzer():
     disk_test()
     with open('veriler.json', 'r') as f:
         veriler = json.load(f)
-    return render_template('index.html', veriler=veriler)
+    return render_template('disk-status.html', veriler=veriler)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
